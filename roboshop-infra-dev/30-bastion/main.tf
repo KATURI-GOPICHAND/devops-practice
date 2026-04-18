752e08998bf70d6057562218d0bc9e190de46547
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-04-18 12:51:30 +0530

    terraform

--- roboshop-infra-dev/30-bastion/main.tf
+++ roboshop-infra-dev/30-bastion/main.tf
@@ -3,8 +3,9 @@ resource "aws_instance" "bastion" {
   instance_type = "t3.micro"
   subnet_id = local.public_subnet_id
   vpc_security_group_ids = [local.bastion_sg_id]
-  iam_instance_profile = aws_iam_instance_profile.bastion.name
- /* user_data = file("bastion.sh")
+  /* iam_instance_profile = aws_iam_instance_profile.bastion.name
+  
+  user_data = file("bastion.sh")
   root_block_device {
     volume_size = 50
     volume_type = "gp3"
@@ -15,7 +16,8 @@ resource "aws_instance" "bastion" {
       },
       local.common_tags
     )
-  }  */
+  }   */
+  
 tags = merge(
     {
         Name = "${var.project}-${var.environment}-bastion"
